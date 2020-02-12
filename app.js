9a3e6791ca54cdbd1139a82070081e3ed6fb8182
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-02-12 10:41:38 -0700

    updated javascript css and index for better ui

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ M.Sidenav.init(sideNav, {});
 // Parallax
 const parallax = document.querySelectorAll(".parallax");
 M.Parallax.init(parallax, {});
+
+// Tabs
+const tabs = document.querySelectorAll(".tabs");
+M.Tabs.init(tabs);
+
+// Scrollspy
+const scrollspy = document.querySelectorAll(".scrollspy");
+M.Scrollspy.init(scrollspy);
